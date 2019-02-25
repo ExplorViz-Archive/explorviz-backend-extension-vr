f2558877c8b5718bc55a64036e144faaaf5ee1e9
dan0x09 <danielkoenig95@web.de>
2019-02-25 17:55:08 +0100

    Make extension somewhat work with 1.3.0 (wip)

--- src/main/java/net/explorviz/extension/vr/main/Application.java
+++ src/main/java/net/explorviz/extension/vr/main/Application.java
@@ -1,18 +1,33 @@
 package net.explorviz.extension.vr.main;
 
-import javax.ws.rs.ApplicationPath;
-
+import net.explorviz.shared.security.filters.AuthenticationFilter;
+import net.explorviz.shared.security.filters.AuthorizationFilter;
+import net.explorviz.shared.security.filters.CorsResponseFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("/extension/dummy")
 public class Application extends ResourceConfig {
 
-	public Application() {
+  public Application() {
 
-		// register DI
-		register(new ExtensionDependencyInjectionBinder());
+    // GenericTypeFinder.getTypeMap().put("DummyModel", DummyModel.class);
+    // GenericTypeFinder.getTypeMap().put("SubDummyModel", SubDummyModel.class);
 
-		// Enable CORS
-		register(CORSResponseFilter.class);
-	}
+    // register DI
+    register(new DependencyInjectionBinder());
+
+    // Security
+    this.register(AuthenticationFilter.class);
+    this.register(AuthorizationFilter.class);
+    this.register(CorsResponseFilter.class);
+
+    // register providers
+    // this.register(JsonApiProvider.class);
+    // this.register(JsonApiListProvider.class);
+
+    // register all resources in the given package
+    // register(TestResource.class);
+
+    // Starting point for your DI-based extension
+    this.register(SetupApplicationListener.class);
+  }
 }
